16985807020f9d9597c59db9d8c10f425723a944
sudhir <sudhir@marcadors.com>
2025-07-18 13:24:24 +0530

    server seup

---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Default middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
+
+
+
+// router
+app.get('/', (req, res)=>{
+    res.send('Hello World')
+})
+
+// express app listen
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
